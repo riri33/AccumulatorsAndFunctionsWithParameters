66f1965afb5903dbd5d1ae007716426b412f0ea0
mccrocjr <mccrocjr@rose-hulman.edu>
2017-09-06 12:41:17 -0400

    finished m8

--- src/m7_why_parameters_are_powerful.py
+++ src/m7_why_parameters_are_powerful.py
@@ -116,7 +116,7 @@ def better_draw_circles(n):
     window.close_on_mouse_click()
 
 # ----------------------------------------------------------------------
-# TODO: 3.
+# Done: 3.
 #   In the previous exercise, you made a MORE POWERFUL version
 #   of draw_circles by introducing a PARAMETER for the amount by
 #   which the radii of the concentric circles increase.
